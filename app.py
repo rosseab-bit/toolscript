0d1b152f577f1ea85e50671fa75511088dd130d2
rosseab-bit <roseab.developer@gmail.com>
2021-04-09 17:42:27 -0300

    agrego opciones de menu

--- app.py
+++ app.py
@@ -2,9 +2,23 @@
 import os 
 import sys
 import json
-
+import time
 db_scripts = 'conf.d/scripts.json'
 script_path = './packages'
+
+banner = """
+▐▓█▀▀▀▀▀▀▀▀▀█▓▌░▄▄▄▄▄░
+▐▓█░░▀░░▀▄░░█▓▌░█▄▄▄█░
+▐▓█░░▄░░▄▀░░█▓▌░█▄▄▄█░
+▐▓█▄▄▄▄▄▄▄▄▄█▓▌░█████░
+░░░░▄▄███▄▄░░░░░█████░
+"""
+print ('='*22)
+print ('@sbitsar')
+print (banner)
+print ('    scriptools')
+print ('='*22)
+print ('Bienvenido a scriptool!')
 
 list_scripts = os.listdir(script_path)
 load_dbScripts = json.loads(open(db_scripts).read())
@@ -13,7 +27,35 @@ dump_list = open(db_scripts, 'w')
 dump_list.write(json.dumps(load_dbScripts, indent=4))
 dump_list.close()
 print (load_dbScripts)
+list_scripts_loads = json.loads(open(db_scripts).read())["scripts"]
+n = 0
 for script in json.loads(open(db_scripts).read())["scripts"]:
-    execute = 'python3 packages/%s' %script
-    os.system(execute)
-
+    print (str(n)+': '+script)
+    n = n+1
+option = input('Desea ejecutar todos los scripts? yes/no ')
+if option == "yes":
+    print ('se ejecutaran todos los script')
+elif option == "no":
+    number_script = input('ingrese el numbero de los scrips a ejecutar separado por una coma ej: 0,1,4 \n')
+    scripts_execute = number_script.split(',')
+    print ('se ejecutaran los siguientes scripts:')
+    print ('-'*20)
+    for script in scripts_execute:
+        print (list_scripts_loads[int(script)])
+    print ('-'*20)
+    option = input('Confirma la ejecucion de los scipts? yes/no: \n')
+    if option == 'yes':
+        print ('Ejecutando scripts')
+        time.sleep(1)
+        print ('Finaliza la ejecucion')
+    elif option == 'no':
+        print ('Se cancela la ejecucion de los scripts')
+        print ('-'*20) 
+        for script in scripts_execute:
+            print(list_scripts_loads[int(script)])
+        print ('-'*20)
+else:
+    print ('no se ingreso ninguna opcion')
+#for script in json.loads(open(db_scripts).read())["scripts"]:
+#    execute = 'python3 packages/%s' %script
+#    os.system(execute)
